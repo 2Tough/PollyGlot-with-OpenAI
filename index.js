d854b137b38cd84016a5371adcf043518fe89308
Polaski <101113655+2Tough@users.noreply.github.com>
2024-04-03 20:17:01 +0300

    Added API connection

--- index.js
+++ index.js
@@ -1,41 +1,70 @@
+import OpenAI from 'openai'
+
 // Languages
 
-const spanishBtn = document.getElementById("input--select--Spanish")
-const frenchBtn = document.getElementById("input--select--French")
-const italianBtn = document.getElementById("input--select--Italian")
+const spanishBtn = document.getElementById("input--select--Spanish");
+const frenchBtn = document.getElementById("input--select--French");
+const italianBtn = document.getElementById("input--select--Italian");
 
 // Translate Btn
 
-const translateBtn = document.getElementsByClassName("translate--btn")
-const translateBtnId = document.getElementById("translate--btn")
+const translateBtn = document.getElementsByClassName("translate--btn");
+const translateBtnId = document.getElementById("translate--btn");
 
 // Input
-const inputText = document.getElementById("input--text")
-
+const inputText = document.getElementById("input--text");
 
 // Event listeners
 
-translateBtnId.addEventListener("click", getText)
+translateBtnId.addEventListener("click", getText);
 
 function getText(e) {
-    e.preventDefault
+    e.preventDefault;
     //console.log(inputText.value)
     if (spanishBtn.checked) {
-        
-        translateData("Spanish")
-    } 
+        fetchData("Spanish");
+    }
     if (frenchBtn.checked) {
-        alert("French");
-        translateData("French")
-    }  
+        fetchData("French");
+    }
     if (italianBtn.checked) {
-        alert("Italian");
-        translateData("Italian")
-    } 
+        fetchData("Italian");
+    }
 }
 
-function translateData(language) {
-if ((language) === "Spanish") {
-    console.log("Translate to Spanish: " + inputText.value);
+async function fetchData(language) {
+    const messages = [
+        {
+            role: 'system',
+            content: 'You a translator assistant. You are provide with input and have to translate it to the language requested.'
+        },
+        {
+            role: 'user',
+            content: `Translate: ${inputText.value} to ${language}`
+        }
+    ]
+
+    try {
+        const openai = new OpenAI({
+            dangerouslyAllowBrowser: true,
+            apiKey: OPENAI_API_KEY,
+        })
+        
+        const response = await openai.chat.completions.create({
+            model: 'gpt-3.5-turbo',
+            temperature: 1.5,
+            messages: messages
+        })
+        renderTranslation(response.choices[0].message.content)
+    } catch (error) {
+        console.log('Error message: ' + error)
+    }
+
 }
+
+function renderTranslation(data) {
+    console.log(data)
+    const outputArea = document.getElementsByClassName("outputArea")
+    const hideLanguages = document.getElementsByClassName("main--app--content--container--languages")
 }
+
